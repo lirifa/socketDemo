8c8b89714b3acbd0c0205601ae1f8ffedbc2ecad
rafa <rafa_im@163.com>
2020-07-31 18:04:50 +0800

    改用ws

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 var app = require('express')();
 var http = require('http').Server(app);
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
 var io = require('socket.io')(http);
+var WebSocketServer = require('ws').Server;
 var port = process.env.PORT || 4000;
+
+const nbs = new WebSocketServer({
+  port: 4001,
+})
+
+
+nbs.on("connection", (ws, req) => {
+  ws.on("message", message => {
+    // 这里解析数据
+    const result = JSON.parse(message);
+    console.log(result);
+    // 是否是登陆，登陆后绑定id
+    if (result.login) {
+      ws.socketIdxos = result.id;
+      nbs.clients.forEach(s => {
+        if (s.socketIdxos !== result.id && s.readyState == 1) {
+          s.send(JSON.stringify({ type: 'login', data: result.id }));
+        }
+      });
+    } else if (result.to) {
+      // 这里只做一个小的场景，不是登陆就是发送信息
+      // 根据存储的id 是否是要 发送的对象 来单独响应
+      nbs.clients.forEach(s => {
+        if (s.socketIdxos == result.to && s.readyState == 1) {
+          s.send(JSON.stringify(result));
+        }
+      });
+    } else {
+      nbs.clients.forEach(s => {
+        s.send(JSON.stringify(result))
+      });
+    }
+  });
+  ws.on("close", message => {
+    console.log(ws.socketIdxos,"退出连接");
+  });
+});
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+app.get('/ws/demo', function(req, res){
+  res.sendFile(__dirname + '/index2.html');
 });
 
 io.on('connection', function(socket){
